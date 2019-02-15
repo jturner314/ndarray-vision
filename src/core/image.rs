8cbe05a2e2b716017f84f76b87e590ef56815f1b
Jim Turner <git@turner.link>
2019-02-14 19:55:06 -0500

    Fix compilation errors

--- src/core/image.rs
+++ src/core/image.rs
@@ -1,7 +1,7 @@
 use crate::core::formats::PixelFormat;
 use ndarray::{ArrayView, ArrayView3, ArrayViewMut, Array3, Axis, Ix1, s, Zip};
 use num_traits::{Zero, One};
-
+use std::ops::Mul;
 
 pub struct Image<T> {
     /// Images are always going to be 3D to handle rows, columns and colour 
@@ -30,20 +30,30 @@ impl<T> Image<T> where T: One + Zero + Clone + Default {
         self.data.slice_mut(s![row, col, ..])
     }
 
-    pub fn conv<U>(&self, kernel: ArrayView3<U>) -> Image<T> {
+    pub fn conv<U: Clone>(&self, kernel: ArrayView3<U>) -> Image<T>
+    where
+        T: Mul<U, Output = T>,
+    {
         Image {
             data: conv(self.data.view(), kernel),
             format: self.format
         }
     }
 
-    pub fn conv_inplace<U>(&mut self, kernel: ArrayView3<U>) {
+    pub fn conv_inplace<U: Clone>(&mut self, kernel: ArrayView3<U>)
+    where
+        T: Mul<U, Output = T>,
+    {
         self.data = conv(self.data.view(), kernel);
     }
 }
 
 
-pub fn conv<T, U>(image: ArrayView3<T>, kernel: ArrayView3<U>) -> Array3<T> where T: Clone + Zero {
+pub fn conv<T, U>(image: ArrayView3<T>, kernel: ArrayView3<U>) -> Array3<T>
+where
+    T: Clone + Zero + Mul<U, Output = T>,
+    U: Clone,
+{
     let mut result = Array3::<T>::zeros(image.dim());
     let k_s = kernel.shape();
     let row_offset = k_s[0]/2;
@@ -51,7 +61,7 @@ pub fn conv<T, U>(image: ArrayView3<T>, kernel: ArrayView3<U>) -> Array3<T> wher
 
     Zip::indexed(image.windows(kernel.dim()))
         .apply(|(i, j, _), window| {
-            let mult =  window * kernel;
+            let mult = &window * &kernel;
             let sums = mult.sum_axis(Axis(2));
             result.slice_mut(s![i+row_offset, j+col_offset, ..]).assign(&sums);
         });
